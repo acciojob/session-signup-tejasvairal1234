a21dea1316073a9c6fa91f291e1f7c95abdb3c00
tejasvairal1234 <tejasvairal7066@gmail.com>
2026-06-05 14:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const form = document.getElementById("signup-form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  const confirmPassword =
+    document.getElementById("confirm-password").value;
+
+  if (password === confirmPassword) {
+    sessionStorage.setItem("name", name);
+    sessionStorage.setItem("email", email);
+    sessionStorage.setItem("password", password);
+
+    alert("Sign up successful!");
+  } else {
+    alert("Passwords do not match");
+  }
+});
